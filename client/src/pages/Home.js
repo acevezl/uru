b5946820eef471f7a8c080414e97325f31145ea0
Arnaut <lou.arnaut@gmail.com>
2021-08-16 19:45:17 -0700

    reset repo

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
+import Auth from '../utils/auth';
+
+const Home = () => {
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Home;
