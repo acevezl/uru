fbacbd2d1c62e122b1eb6bf3eac48a6d0bb0b0fc
Cerebreo <nellirism@outlook.com>
2021-08-19 21:28:40 -0700

    favicon attachment

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="py-3 my-4 mx-4">
         <ul className="nav justify-content-center border-bottom p-3 mb-3">
-          <li className="nav-item" style={{ color: '#69b4d4'}}>"Today, You are You, that is Truer than True, There is no one alive, that is Youer than You" - Dr. Seuss</li>
+          <li className="nav-item" style={{ color: '#69b4d4'}}>"Today, You are You, that is Truer than True, There is no one alive, that is Youer than You" ~ Dr. Seuss</li>
         </ul>
         <p className="text-center"><a href="https://github.com/acevezl/uru" target="_blank" rel="noopener noreferrer" style={{ color: '#69b4d4'}}><Github></Github></a></p>
         
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
 
 //import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
-import { useMutation } from '@apollo/react-hooks';
-import { ADD_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   // set state for form validation
   const [validated] = useState(false);
 
@@ -37,7 +41,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({
-        variables: { ...userFormData }
+        variables: { ...userFormData },
       });
 
       Auth.login(data.addUser.token);
@@ -46,9 +50,9 @@ const SignupForm = () => {
     }
 
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     });
   };
 
@@ -57,91 +61,117 @@ const SignupForm = () => {
       {/* This is needed for the validation functionality above */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         {/* show alert if server response is bad */}
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
           Something went wrong with your signup!
         </Alert>
 
+       
+
         <Form.Group>
-          <Form.Label htmlFor='username'>Username</Form.Label>
+          <Form.Label htmlFor="username">Username</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
+            type="text"
+            placeholder="Your username"
+            name="username"
             onChange={handleInputChange}
             value={userFormData.username}
             required
           />
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='first_name'>First Name</Form.Label>
+          <Form.Label htmlFor="first_name">First Name</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Your first name'
-            name='first_name'
+            type="text"
+            placeholder="Your first name"
+            name="first_name"
             onChange={handleInputChange}
             value={userFormData.first_name}
             required
           />
-          <Form.Control.Feedback type='invalid'>First name is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            First name is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='last_name'>Last Name</Form.Label>
+          <Form.Label htmlFor="last_name">Last Name</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Your last name'
-            name='last_name'
+            type="text"
+            placeholder="Your last name"
+            name="last_name"
             onChange={handleInputChange}
             value={userFormData.last_name}
             required
           />
-          <Form.Control.Feedback type='invalid'>Last name is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Last name is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='phone'>Phone Number</Form.Label>
+          <Form.Label htmlFor="phone">Phone Number</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Your phone number'
-            name='phone'
+            type="text"
+            placeholder="Your phone number"
+            name="phone"
             onChange={handleInputChange}
             value={userFormData.phone}
             required
           />
-          <Form.Control.Feedback type='invalid'>Your phone number is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Your phone number is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
+            type="email"
+            placeholder="Your email address"
+            name="email"
             onChange={handleInputChange}
             value={userFormData.email}
             required
           />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
+            type="password"
+            placeholder="Your password"
+            name="password"
             onChange={handleInputChange}
             value={userFormData.password}
             required
           />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <Button
-          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
+          disabled={
+            !(
+              userFormData.username &&
+              userFormData.email &&
+              userFormData.password
+            )
+          }
+          type="submit"
+          variant="success"
+        >
           Submit
         </Button>
       </Form>
