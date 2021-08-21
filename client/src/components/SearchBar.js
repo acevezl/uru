456de57ff36448a8ac917c3db28b9dd771be24a1
Cerebreo <nellirism@outlook.com>
2021-08-21 15:42:44 -0700

    debugging

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,25 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Row, Col, Form, InputGroup, FormControl, Button } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_THERAPISTS_CRITERIA } from "../utils/queries";
+import TherapistList from "../components/TherapistList";
 
 const SearchBar = () => {
-    return(
-        <>
-            <Row className='text-center my-4'>
-                <Col><h1 style={{color: '#69b4d4'}}>Let us help you find a Therapist for your child!</h1></Col>
-            </Row>
-            <Row>
-                <Form onSubmit={handleFormSubmit}>
-                    <InputGroup className="mb-3 mx-auto" style={{ width: '75%' }} type='search'>
-                    <FormControl
-                    placeholder="Seach for a specialty..." className='mx-2'
-                    />
-                    <Button variant="outline-warning">Search</Button>{' '}
-                    </InputGroup>
-                </Form>
-            </Row>
-        </>
-    )
-}
+  const [searchText, setSearchText] = useState("");
 
-export default SearchBar; 
+  const { loading, data } = useQuery(QUERY_THERAPISTS_CRITERIA, {
+    variables: { criteria: searchText },
+  });
+
+  const therapists = data?.therapistcriteria || [];
+  // update state based on form input changes
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchText(event.target.searchText.value);
+    console.log(event.target.searchText.value);
+  };
+
+  return (
+    <>
+      <Row className="text-center my-4 col-12">
+        <Col>
+          <h1 style={{ color: "#116c95" }}>
+            Let us help you find a Therapist for your child!
+          </h1>
+        </Col>
+      </Row>
+      <Row className="text-center my-4 col-12">
+        <Form onSubmit={handleSubmit} className="col-12">
+          <InputGroup
+            className="mb-3 mx-auto"
+            style={{ width: "75%" }}
+            type="search"
+          >
+            <FormControl
+              name="searchText"
+              placeholder="Seach for a specialty..."
+              className="mx-2"
+            />
+            <Button type="submit" variant="outline-warning">
+              Search
+            </Button>{" "}
+          </InputGroup>
+        </Form>
+      </Row>
+      {/* therapist cards, need to be conditionally rendered */}
+      <Row>
+        {loading ? (
+          <div>Loading therapists...</div>
+        ) : (
+          <TherapistList therapists={therapists} />
+        )}
+      </Row>
+    </>
+  );
+};
+
+export default SearchBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,66 +10,13 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_THERAPISTS_CRITERIA } from "../utils/queries";
-import TherapistList from "../components/TherapistList";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
-  const [searchText, setSearchText] = useState("");
-
-  const { loading, data } = useQuery(QUERY_THERAPISTS_CRITERIA, {
-    variables: { criteria: searchText },
-  });
-
-  const therapists = data?.therapistcriteria || [];
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    setSearchText(event.target.value);
-  };
-
-  const handleSearch = async (event) => {
-    event.preventDefault();
-  };
-
   return (
     <>
       <Container>
-        <>
-          <Row className="text-center my-4 col-12">
-            <Col>
-              <h1 style={{ color: "#116c95" }}>
-                Let us help you find a Therapist for your child!
-              </h1>
-            </Col>
-          </Row>
-          <Row className="text-center my-4 col-12">
-            <Form onSubmit={handleSearch} className="col-12">
-              <InputGroup
-                className="mb-3 mx-auto"
-                style={{ width: "75%" }}
-                type="search"
-              >
-                <FormControl
-                  name="searchText"
-                  placeholder="Seach for a specialty..."
-                  className="mx-2"
-                  onChange={handleChange}
-                />
-                <Button variant="outline-warning">Search</Button>{" "}
-              </InputGroup>
-            </Form>
-          </Row>
-        </>
-        {/* therapist cards, need to be conditionally rendered */}
-        <Row>
-          {loading ? (
-            <div>Loading therapists...</div>
-          ) : (
-            <TherapistList therapists={therapists} />
-          )}
-        </Row>
+        <SearchBar></SearchBar>
       </Container>
     </>
   );
